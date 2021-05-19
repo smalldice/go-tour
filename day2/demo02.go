69e2582bba894036b8c64221cd4e66cdaf188c64
smalldice <361099307@qq.com>
2021-05-19 23:04:43 +0800

    feat: 添加go day2 demo

---
+++ day2/demo02.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main () {
+	var a int = 1;
+	var c, java, python = false, 2, "no"
+
+	fmt.Println(a)
+	fmt.Println(c, java, python)
+}
---
+++ day2/demo03.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main () {
+	k := 2
+	fmt.Println(k)
+}
