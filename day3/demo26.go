dc9996cd21c8335f55b75776c118a121252bf671
smalldice <361099307@qq.com>
2021-05-21 13:43:43 +0800

    feat: add demo

---
+++ day3/demo26.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func main() {
+	// range pow 返回切片的 下标 和 下标对应的value
+	for i, v := range pow {
+		fmt.Printf("2 ** %d = %d \n", i, v)
+	}
+
+	// 可以用 _ 来忽略下标或者 值
+	for _, v := range pow {
+		fmt.Printf("%d\n", v)
+	}
+
+	for i := range pow {
+		fmt.Printf("%d\n", i)
+	}
+}
